2ebc099236cb8e402f288dbbe698621a25952341
salahghr4 <salahghr4@gmail.com>
2025-05-07 20:14:15 +0100

    feat(client): add Sonner toaster component

--- client/src/providers/AppProvider.tsx
+++ client/src/providers/AppProvider.tsx
@@ -1,10 +1,12 @@
 import { AuthProvider } from "@/context/AuthContext";
 import { ThemeProvider } from "@/context/ThemeContext";
+import { Toaster } from "@/components/ui/sonner"
 
 const AppProvider = ({ children }: { children: React.ReactNode }) => {
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <AuthProvider>{children}</AuthProvider>
+      <Toaster richColors expand closeButton/>
     </ThemeProvider>
   );
 };
