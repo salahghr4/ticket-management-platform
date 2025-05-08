9a25c246b43016bc8fa67f28940f4f07ed7f6bc2
salahghr4 <salahghr4@gmail.com>
2025-05-08 19:58:19 +0100

    fix(client logo): update logo selection based on system theme

--- client/src/components/Logo/SidebarLogo.tsx
+++ client/src/components/Logo/SidebarLogo.tsx
@@ -1,12 +1,22 @@
 import Logo from "@/components/Logo/Logo";
 import logoIcon from "@/assets/imgs/logo.png";
+import logoWhite from "@/assets/imgs/LogoWhite.png";
+import { useTheme } from "@/hooks/useTheme";
 
 const SidebarLogo = ({ sideBarOpen }: { sideBarOpen: boolean }) => {
+  const { theme } = useTheme();
+  // handle system theme
+  const systemTheme = window.matchMedia("(prefers-color-scheme: dark)").matches
+    ? "dark"
+    : "light";
+
+  const resolvedTheme = theme === "system" ? systemTheme : theme;
+  const selectedLogo = resolvedTheme === "dark" ? logoWhite : logoIcon;
   return sideBarOpen ? (
     <Logo />
   ) : (
     <img
-      src={logoIcon}
+      src={selectedLogo}
       alt="TYTICKET logo"
     />
   );
